f0bf4a699fa7f2b37387738ca37044d80945b675
Chhuong Le <chhuongle@gmail.com>
2023-01-21 10:29:11 -0800

    add components to header

--- airbnb-clone/components/Header.jsx
+++ airbnb-clone/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '../public/airbnb-logo.png'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { BiGlobe } from 'react-icons/bi'
 
 export default function Header(){
   return (
@@ -16,10 +17,17 @@ export default function Header(){
       </div>
       {/* Search Bar */}
       <div className="flex items-center border-2 rounded-full md:shadow-md" >
-        <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent outline-none text-sm md:text-lg" />
+        <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent outline-none text-sm md:text-lg text-gray-600 placeholder-gray-400" />
         <AiOutlineSearch size={30} style={{ padding: "5px" }} className="hidden md:inline-flex md:mx-2 bg-[#FF5A5F] text-white rounded-full cursor-pointer "/>
       </div>
       {/* Right side */}
+      <div className="flex items-center px-3">
+        <p className="px-5">Airbnb your home</p>
+        <BiGlobe size={20} style={{ paddingRight: "3px" }} />
+        <div>
+          
+        </div>
+      </div>
     </header>
   )
 }
