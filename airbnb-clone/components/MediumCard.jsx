b1a676c8f6e1bf923cb320e786b1de3210fe51c1
Chhuong Le <chhuongle@gmail.com>
2023-01-24 11:23:23 -0800

    remove console logs from medium and small card components

--- airbnb-clone/components/MediumCard.jsx
+++ airbnb-clone/components/MediumCard.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 
 export default function MediumCard({ image, category }) {
-  console.log(category)
   return (
     <div className="rounded-xl cursor-pointer hover:scale-105 transition transform duration-300 ease-out">
       <div className="relative h-80 w-80">
