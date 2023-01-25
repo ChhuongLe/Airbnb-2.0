6cfece48a3f639922c6c10dabb46cde260f0a2fd
Chhuong Le <chhuongle@gmail.com>
2023-01-25 13:06:53 -0800

    add second page into developement

---
+++ airbnb-clone/pages/search.js
@@ -0,0 +1,17 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+export default function Search() {
+  return(
+    <div className="h-screen">
+      <Header />
+      <main>
+        <section>
+          <p>300+ stays for 5 guests</p>
+
+          <h1>Stays in Mars</h1>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
