12e02b4e81aef7cf32fda88633372ca43e7f82dc
Chhuong Le <chhuongle@gmail.com>
2023-02-01 10:57:24 -0800

    fix sql schema to only load specific data and set up postgres connection to nextjs app

--- airbnb-clone/postgres.sql
+++ airbnb-clone/postgres.sql
@@ -1,22 +1,28 @@
 DROP TABLE IF EXISTS
-"listings"
+"listings";
 
 CREATE TABLE "listings" (
-  "id" INTEGER NOT NULL,
+  "id" BIGINT NOT NULL,
   "listing_url" TEXT,
+  "title" TEXT,
+  "description" TEXT,
   "image_url" TEXT,
   "host_name" TEXT,
-  "title" TEXT,
-  "star" INTEGER,
-  "description" TEXT,
+  "host_url" TEXT,
+  "neighborhood" TEXT,
+  "long" REAL DEFAULT 0,
+  "lat" REAL DEFAULT 0,
+  "property_type" TEXT,
+  "room_type" TEXT,
+  "accomodates" SMALLINT DEFAULT 1,
+  "num_bedrooms" SMALLINT DEFAULT 1,
+  "num_beds" SMALLINT DEFAULT 1,
   "price" TEXT,
-  "long" INTEGER,
-  "lat" INTEGER,
-  PRIMARY KEY("id"),
-)
+  "rating" REAL DEFAULT 1
+);
 
 -- Load data into listings table
-COPY "listings"("id", "listing_url", "image_url", "host_name", "title", "star", "long", "lat")
-from '~/listings/listings.csv'
-DELIMITER ','
+COPY "listings"("id", "listing_url", "title", "description", "image_url", "host_name", "host_url", "neighborhood", "long", "lat", "property_type", "room_type", "accomodates", "num_bedrooms", "num_beds", "price", "rating")
+from '/home/chhuongle/listings/listings_edit.csv'
+WITH DELIMITER ','
 CSV HEADER;
