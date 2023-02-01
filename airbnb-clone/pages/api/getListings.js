d450d485cf4a24e97ad39cd82839a27c11ba12eb
Chhuong Le <chhuongle@gmail.com>
2023-02-01 11:54:27 -0800

    add route to get listings from postgres db

---
+++ airbnb-clone/pages/api/getListings.js
@@ -0,0 +1,7 @@
+import conn from '../../lib/db.js';
+
+export default async function handler(req, res) {
+  const query = `SELECT * FROM listings`
+  const data = await conn.query();
+  res.status(200).send(data);
+}
