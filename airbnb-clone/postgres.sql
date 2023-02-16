80aa645b08d21e60de4eb797a61c66b28f709ca7
Chhuong Le <chhuongle@gmail.com>
2023-02-16 13:50:46 -0800

    change sql schema to fit data

--- airbnb-clone/postgres.sql
+++ airbnb-clone/postgres.sql
@@ -5,9 +5,9 @@ CREATE TABLE "listings" (
   "city_id" INTEGER NOT NULL,
   "city" TEXT,
   "listing_url" TEXT,
-  "title" TEXT,
-  "description" TEXT,
-  "image_url" TEXT,
+  "name" TEXT,
+  "neighborhood_overview"  TEXT,
+  "picture_url" TEXT,
   "host_name" TEXT,
   "host_url" TEXT,
   "neighborhood" TEXT,
@@ -16,15 +16,17 @@ CREATE TABLE "listings" (
   "property_type" TEXT,
   "room_type" TEXT,
   "accomodates" SMALLINT DEFAULT 1,
+  "num_bathrooms" TEXT,
   "num_bedrooms" SMALLINT DEFAULT 1,
   "num_beds" SMALLINT DEFAULT 1,
+  "amenities" TEXT [],
   "price" TEXT,
   "rating" REAL DEFAULT 1,
   PRIMARY KEY ("id")
 );
 
 -- Load data into listings table
-COPY "listings"("id","city_id","city","listing_url","name", "neighborhood_overview", "picture_url", "host_name", "host_url", "neighborhood", "long", "lat", "property_type", "room_type", "accomodates", "num_bedrooms", "num_beds", "amenities", "price", "rating")
+COPY "listings"("id","city_id","city","listing_url","name", "neighborhood_overview", "picture_url", "host_name", "host_url", "neighborhood", "long", "lat", "property_type", "room_type", "accomodates", "num_bathrooms","num_bedrooms", "num_beds", "amenities", "price", "rating")
 FROM '/home/chhuongle/listings/listingdata.csv'
 WITH DELIMITER ','
 CSV HEADER;
